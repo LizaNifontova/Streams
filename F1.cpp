a60960596529098216407ad6df416f36efa15f8e
LizaNifontova <47351467+LizaNifontova@users.noreply.github.com>
2021-10-07 11:55:48 +0500

    Add files via upload

---
+++ F1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h> 
+#include <fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int StartMs = GetTickCount(); 
+
+    std::ofstream ofs("1.txt");
+    for (int i = 0; i < 100000; ++i) 
+        ofs << i << ' ';
+
+    int EndMs = GetTickCount();
+
+    std::cout << "Время выполнения: " << EndMs - StartMs << " ms\n";
+    std::getchar();
+    return 0;
+}
